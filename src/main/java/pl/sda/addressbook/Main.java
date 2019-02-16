73dbce8da1ce213db301475d3a84d9cf068c59c2
kamisamapl <jagla1990@gmail.com>
2019-02-16 10:32:54 +0100

    Dodawanie do repozytorium

---
+++ src/main/java/pl/sda/addressbook/Main.java
@@ -0,0 +1,19 @@
+package pl.sda.addressbook;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+    launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/VerticalBox.fxml"));
+
+    }
+}
